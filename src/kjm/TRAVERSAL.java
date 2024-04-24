d018581fab3c2392752a2c6d04ee7244bc0cc2c1
jay <woalskdl@gmail.com>
2024-04-25 08:36:05 +0900

    TRAVERSAL 임시 커맛

---
+++ src/kjm/TRAVERSAL.java
@@ -0,0 +1,19 @@
+package kjm;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class TRAVERSAL {
+    private static int N;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int C = Integer.parseInt(br.readLine());
+
+        while (C-- > 0) {
+
+        }
+    }
+}
