52eebe423ffb5e6416f427146d15e051b03b3c1b
jay <woalskdl@gmail.com>
2024-04-25 21:06:29 +0900

    TRAVERSAL 실패

--- src/kjm/TRAVERSAL.java
+++ src/kjm/TRAVERSAL.java
@@ -1,49 +1,14 @@
 package kjm;
 
-import org.w3c.dom.Node;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 
-/*
-전위 순회는 맨 처음에 트리의 루트를 방문하고, 왼쪽과 오른쪽 서브트리를 순서대로 방문합니다.
-중위 순회는 왼쪽과 오른쪽 서브트리 사이에 트리의 루트를 방문하고, 후위 순회는 왼쪽과 오른쪽 서브트리를 모두 방문한 뒤에야 루트를 방문합니다.
-
-다음 그림은 이진 트리의 한 예를 보여 줍니다. 이 트리를 전위 순회하면 모든 노드를 27, 16, 9, 12, 54, 36, 72의 순서대로 방문하게 됩니다.
-반면 중위 순회했을 때는 9, 12, 16, 27, 36, 54, 72의 순서로, 후위 순회했을 때는 12, 9, 16, 36, 72, 54, 27의 순서로 방문하게 되지요.
-
-
-어떤 이진 트리를 전위 순회했을 때 노드들의 방문 순서와, 중위 순회했을 때 노드들의 방문 순서가 주어졌다고 합시다.
-이 트리를 후위 순회했을 때 각 노드들을 어떤 순서대로 방문하게 될지 계산하는 프로그램을 작성하세요.
-
-입력
-입력의 첫 줄에는 테스트 케이스의 수 C (1≤C≤100)가 주어집니다. 각 테스트 케이스는 세 줄로 구성되며, 첫 줄에는 트리에 포함된 노드의 수 N (1≤N≤100)이 주어집니다.
-그 후 두 줄에 각각 트리를 전위 순회했을 때와 중위순회 했을 때의 노드 방문 순서가 N개의 정수로 주어집니다.
-각 노드는 1000 이하의 자연수로 번호 매겨져 있으며, 한 트리에서 두 노드의 번호가 같은 일은 없습니다.
-
-출력
-각 테스트 케이스마다, 한 줄에 해당 트리의 후위 순회했을 때 노드들의 방문 순서를 출력합니다.
-
-예제 입력
-2
-7
-27 16 9 12 54 36 72
-9 12 16 27 36 54 72
-6
-409 479 10 838 150 441
-409 10 479 150 838 441
-예제 출력
-12 9 16 36 72 54 27
-10 150 441 838 479 409
-
- */
 public class TRAVERSAL {
     private static int N;
     private static int[][] nodes;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
@@ -73,31 +38,40 @@ public class TRAVERSAL {
                 }
             }
 
+            Node node = new Node();
+            for (int i = 0; i < N; i++) {
+                if (nodes[0][i] != 0)
+                    node = makeNode(0, i);
+            }
 
-
+            printRear(node, sb);
+            sb.append("\n");
         }
+
+        System.out.println(sb);
     }
 
     private static Node makeNode(int top, int left) {
         Node node = new Node();
         node.value = nodes[top][left];
 
-        boolean notFound = true;
-        for (int i = top + 1; i < N && notFound; i++) {
-            for (int j = 0; j < left && notFound; j++) {
-                if (nodes[i][j] != 0) {
-                    node.left = makeNode(i, j);
-                    notFound = false;
+        boolean found = false;
+        if (top + 1 < N) {
+            for (int j = 0; j < left && !found; j++) {
+                if (nodes[top + 1][j] != 0) {
+                    node.left = makeNode(top + 1, j);
+                    found = true;
                 }
             }
         }
 
-        notFound = true;
-        for (int i = top + 1; i < N && notFound; i++) {
-            for (int j = left + 1; j < N && notFound; j++) {
-                if (nodes[i][j] != 0) {
-                    node.right = makeNode(i, j);
-                    notFound = false;
+        int y = found ? node.y : top;
+        if (y + 1 < N) {
+            found = false;
+            for (int j = left + 1; j < N && !found; j++) {
+                if (nodes[y + 1][j] != 0) {
+                    node.right = makeNode(y + 1, j);
+                    found = true;
                 }
             }
         }
@@ -105,8 +79,22 @@ public class TRAVERSAL {
         return node;
     }
 
+    private static void printRear(Node node, StringBuilder sb) {
+        if (node.left == null && node.right == null)
+            sb.append(node.value).append(" ");
+        else if (node.left != null)
+            printRear(node.left, sb);
+        else
+            printRear(node.right, sb);
+
+        sb.append(node.value).append(" ");
+    }
+
     static class Node {
         int value;
+        int y;
+        int x;
+
         Node left;
         Node right;
     }
