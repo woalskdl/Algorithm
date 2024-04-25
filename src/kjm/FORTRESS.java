5389438b5c9ac48f09a6175577ead768364ea892
jay <woalskdl@gmail.com>
2024-04-26 08:15:47 +0900

    FORTRESS 잘푼거같은데 오답중..

--- src/kjm/FORTRESS.java
+++ src/kjm/FORTRESS.java
@@ -3,9 +3,11 @@ package kjm;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class FORTRESS {
-    private static int[][] map;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
@@ -13,24 +15,101 @@ public class FORTRESS {
         int C = Integer.parseInt(br.readLine());
 
         while (C-- > 0) {
-            int N = Integer.parseInt(br.readLine());
-            map = new int[1001][1001];
+            int N = Integer.parseInt(br.readLine().replaceAll(" ", ""));
 
-            for (int i = 0; i < N; i++) {
-                String input = br.readLine();
-                String[] inputs = input.split(" ");
+            String input = br.readLine();
+            String[] inputs = input.split(" ");
 
-                int x = Integer.parseInt(inputs[0]);
-                int y = Integer.parseInt(inputs[1]);
-                int r = Integer.parseInt(inputs[2]);
+            int x = Integer.parseInt(inputs[0]);
+            int y = Integer.parseInt(inputs[1]);
+            int r = Integer.parseInt(inputs[2]);
 
-                for (int j = 0; j < 1001; j++) {
-                    for (int k = 0; k < 1001; k++) {
-                        if (j + k <= x + y + r) // 이거 아님
-                            map[j][k] = i + 1;
-                    }
-                }
+            Castle castle = new Castle();
+            castle.x = x;
+            castle.y = y;
+            castle.r = r;
+            castle.depth = 0;
+
+            for (int i = 1; i < N; i++) {
+                input = br.readLine();
+                inputs = input.split(" ");
+
+                x = Integer.parseInt(inputs[0]);
+                y = Integer.parseInt(inputs[1]);
+                r = Integer.parseInt(inputs[2]);
+
+                castle(x, y, r, castle);
+            }
+
+            List<Integer> depthList = new ArrayList<>();
+
+            deepest(castle, depthList);
+
+            depthList.sort(Collections.reverseOrder());
+
+            if (depthList.size() == 1)
+                sb.append(depthList.get(0));
+            else if (depthList.isEmpty())
+                sb.append(0);
+            else
+                sb.append(Math.max(depthList.get(0) + depthList.get(1), depthList.get(0)));
+
+            sb.append("\n");
+        }
+
+        System.out.println(sb);
+    }
+
+    private static void deepest(Castle castle, List<Integer> depthList) {
+        if (castle.castleList.isEmpty()) {
+            depthList.add(castle.depth);
+            return;
+        }
+
+        for (Castle c : castle.castleList)
+            deepest(c, depthList);
+    }
+
+    private static void castle(int x, int y, int r, Castle parent) {
+        if (parent.castleList.isEmpty()) {
+            Castle c = new Castle();
+            c.x = x;
+            c.y = y;
+            c.r = r;
+            c.depth = parent.depth + 1;
+
+            parent.castleList.add(c);
+            return;
+        }
+
+        boolean found = false;
+        for (Castle c : parent.castleList) {
+            double distance = Math.pow(x - c.x, 2) + Math.pow(y - c.y, 2);
+            double biggerR = Math.pow(Math.max(r, c.r), 2);
+
+            if (distance < biggerR) {
+                castle(x, y, r, c);
+                found = true;
+                break;
             }
         }
+
+        if (!found) {
+            Castle c = new Castle();
+            c.x = x;
+            c.y = y;
+            c.r = r;
+            c.depth = parent.depth + 1;
+
+            parent.castleList.add(c);
+        }
+    }
+
+    static class Castle {
+        int x;
+        int y;
+        int r;
+        int depth;
+        List<Castle> castleList = new ArrayList<>();
     }
 }
