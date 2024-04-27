5b22cc8219f2c85db34318ef2a007dd1d00e2603
jay <woalskdl@gmail.com>
2024-04-27 12:23:29 +0900

    RUNNINGMEDIAN

--- src/kjm/test.java
+++ src/kjm/test.java
@@ -10,12 +10,23 @@ public class test {
         list.add(1);
         list.add(2);
         list.add(3);
-        list.add(4);
         list.add(5);
+        list.add(6);
 
-        list.remove(3);
-
+        System.out.println(findMiddle(7, list));
         System.out.println(list);
+    }
 
+    private static int findMiddle(int value, List<Integer> sortedList) {
+        for (int i = sortedList.size() - 1; i >= 0; i--) {
+            if (value > sortedList.get(i)) {
+                sortedList.add(i + 1, value);
+                break;
+            }
+        }
+
+        int idx = sortedList.size() / 2 - Math.abs(sortedList.size() % 2 - 1);
+
+        return sortedList.get(idx);
     }
 }
